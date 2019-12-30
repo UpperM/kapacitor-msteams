31f1a3164f7dc8a35087fd1332144c0e16f4f02d
Matthieu C <courtois.matthieu.92@gmail.com>
2019-12-30 19:15:40 +0100

    Create sendTeamsNotification.sh

---
+++ sendTeamsNotification.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+webhook="YOUR MS TEAMS WEBHOOK"
+
+# Get data send by Kapacitor in STDIN
+kapacitorInfos="$(< /dev/stdin)"
+
+# Extract fields
+details=$(echo $kapacitorInfos | jq .details | sed 's/\"//g')
+title=$(echo $kapacitorInfos | jq .message | sed 's/\"//g')
+level=$(echo $kapacitorInfos | jq .level | sed 's/\"//g')
+
+# Get the correct color for MS Teams
+case "$level" in
+   'OK')
+     themeColor="34cc25"
+     ;;
+   'INFO')
+     themeColor="0076D7"
+     ;;
+   'WARNING')
+     themeColor="ffa533"
+     ;;
+   'CRITICAL')
+     themeColor="CC4A31"
+     ;;
+esac
+
+# Prepare the body for curl
+body=$(jq -n --arg themeColor "$themeColor" --arg text "$details" --arg title "$title" '{text:$text,title:$title,themeColor:$themeColor}')
+
+# POST to MS Teams
+curl --data "$body" -H "Content-Type: application/json" "$webhook"
